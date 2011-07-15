701d4882e211bc57d757ee3c36e42a05af3dc8bd
rosiro <akumapyon@gmail.com>
2011-07-15 17:58:11 +0900

    new file:   Changes 	new file:   MANIFEST 	new file:   MANIFEST.SKIP 	new file:   META.yml 	new file:   Makefile 	new file:   Makefile.PL 	new file:   Makefile.old 	new file:   README 	new file:   blib/arch/.exists 	new file:   blib/arch/auto/Amon2/Setup/Flavor/Rosiro/.exists 	new file:   blib/bin/.exists 	new file:   blib/lib/Amon2/Setup/Asset/BlueTrip.pm 	new file:   blib/lib/Amon2/Setup/Asset/BlueTrip/css/ie.css 	new file:   blib/lib/Amon2/Setup/Asset/BlueTrip/css/print.css 	new file:   blib/lib/Amon2/Setup/Asset/BlueTrip/css/screen.css 	new file:   blib/lib/Amon2/Setup/Asset/BlueTrip/css/style.css 	new file:   blib/lib/Amon2/Setup/Flavor/.exists 	new file:   blib/lib/Amon2/Setup/Flavor/Rosiro.pm 	new file:   blib/lib/auto/Amon2/Setup/Flavor/Rosiro/.exists 	new file:   blib/man1/.exists 	new file:   blib/man3/.exists 	new file:   blib/man3/Amon2::Setup::Flavor::Rosiro.3pm 	new file:   blib/script/.exists 	new file:   inc/Module/Install.pm 	new file:   inc/Module/Install/AuthorTests.pm 	new file:   inc/Module/Install/Base.pm 	new file:   inc/Module/Install/Can.pm 	new file:   inc/Module/Install/Fetch.pm 	new file:   inc/Module/Install/Include.pm 	new file:   inc/Module/Install/Makefile.pm 	new file:   inc/Module/Install/Metadata.pm 	new file:   inc/Module/Install/Repository.pm 	new file:   inc/Module/Install/Win32.pm 	new file:   inc/Module/Install/WriteAll.pm 	new file:   inc/Test/More.pm 	new file:   lib/Amon2/Setup/Asset/BlueTrip.pm 	new file:   lib/Amon2/Setup/Asset/BlueTrip/css/ie.css 	new file:   lib/Amon2/Setup/Asset/BlueTrip/css/print.css 	new file:   lib/Amon2/Setup/Asset/BlueTrip/css/screen.css 	new file:   lib/Amon2/Setup/Asset/BlueTrip/css/style.css 	new file:   lib/Amon2/Setup/Flavor/Rosiro.pm 	new file:   lib/Amon2/Setup/Flavor/Rosiro.pm~ 	new file:   pm_to_blib 	new file:   t/00_compile.t 	new file:   xt/01_podspell.t 	new file:   xt/02_perlcritic.t 	new file:   xt/03_pod.t 	new file:   xt/perlcriticrc

---
+++ blib/lib/Amon2/Setup/Asset/BlueTrip.pm
@@ -0,0 +1,12 @@
+package Amon2::Setup::Asset::BlueTrip;
+use strict;
+use warnings;
+use File::Spec::Functions;
+
+my ($vol, $dir, $file) = File::Spec->splitpath($INC{"Amon2/Setup/Asset/BlueTrip.pm"});
+
+sub bluetrip_path {
+    return catdir($dir, "BlueTrip/css");
+}
+
+1;
